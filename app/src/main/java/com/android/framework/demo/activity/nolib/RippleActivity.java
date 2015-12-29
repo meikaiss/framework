d0b2b881aa061222bc064f601e2297bef2e11d41
meikai <347287812@qq.com>
2015-12-29 20:19:25 +0800

    增加代码创建RippleDrawable的用例

--- app/src/main/java/com/android/framework/demo/activity/nolib/RippleActivity.java
+++ app/src/main/java/com/android/framework/demo/activity/nolib/RippleActivity.java
@@ -1,8 +1,13 @@
 package com.android.framework.demo.activity.nolib;
 
 import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.RippleDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -18,6 +23,7 @@ import java.util.Map;
  */
 public class RippleActivity extends Activity {
 
+    private Button btnNewRipple;
     private ListView listView;
 
     @Override
@@ -33,6 +39,30 @@ public class RippleActivity extends Activity {
 
         final SimpleAdapter adapter = new SimpleAdapter(this, dataList, R.layout.item_demo, new String[]{"name"}, new int[]{R.id.tv_item_name});
         listView.setAdapter(adapter);
+
+        btnNewRipple = (Button) findViewById(R.id.btn_code_new_ripple);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ){
+            ColorStateList colorStateList = new ColorStateList(
+                    new int[][]{
+                            new int[]{android.R.attr.state_pressed},
+                            new int[]{android.R.attr.state_focused},
+                            new int[]{android.R.attr.state_activated},
+                            new int[]{}
+                    },
+                    new int[]{
+                            Color.parseColor("#e2e2e2"),
+                            Color.parseColor("#e2e2e2"),
+                            Color.parseColor("#e2e2e2"),
+                            Color.parseColor("#fafafa")
+                    }
+            );
+            RippleDrawable rippleDrawable = new RippleDrawable(colorStateList, null, null);
+            btnNewRipple.setBackground(rippleDrawable);
+        }else{
+
+        }
+
     }
 
 
