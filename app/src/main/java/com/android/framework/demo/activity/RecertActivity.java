bb4ad67c91782e66f41ff48529bcb53a04b8678c
meikai <347287812@qq.com>
2017-08-11 14:53:49 +0800

    新增一个测试revert

---
+++ app/src/main/java/com/android/framework/demo/activity/RecertActivity.java
@@ -0,0 +1,36 @@
+package com.android.framework.demo.activity;
+
+import android.content.Intent;
+
+import com.android.framework.base.BaseCompactActivity;
+
+/**
+ * Created by meikai on 17/8/11.
+ */
+public class RecertActivity extends BaseCompactActivity {
+    
+    @Override
+    public int getContentViewLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void findViews() {
+
+    }
+
+    @Override
+    public void setListeners() {
+
+    }
+
+    @Override
+    public void parseBundle(Intent intent) {
+
+    }
+
+    @Override
+    public void afterView() {
+
+    }
+}
