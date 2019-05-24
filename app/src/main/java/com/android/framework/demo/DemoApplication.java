6819eddba75b85e4adeef70a727093504d9f67d6
meikaiss <347287812@qq.com>
2019-05-24 09:33:48 +0800

    增加finalize time out解决方案

--- app/src/main/java/com/android/framework/demo/DemoApplication.java
+++ app/src/main/java/com/android/framework/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.android.framework.demo;
 import android.app.Application;
 import android.util.Log;
 
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by meikai on 15/10/28.
  */
@@ -10,7 +12,7 @@ public class DemoApplication extends Application {
 
     private static DemoApplication instance;
 
-    public static DemoApplication getInstance(){
+    public static DemoApplication getInstance() {
         return instance;
     }
 
@@ -20,5 +22,18 @@ public class DemoApplication extends Application {
         instance = this;
 
         Log.e("DemoApplication", "DemoApplication_onCreate");
+
+        Thread.UncaughtExceptionHandler uncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                if (t.getName().equals("FinalizerWatchdogDaemon")
+                        && e instanceof TimeoutException) {
+                    // 无视此异常
+                } else {
+                    uncaughtExceptionHandler.uncaughtException(t, e);
+                }
+            }
+        });
     }
 }
