244f4706cf2a44987ea0ffd07e22804a80da54c8
meikaiss <347287812@qq.com>
2019-06-18 11:14:42 +0800

    探究textView多行居中的问题

---
+++ app/src/main/java/com/android/framework/demo/activity/textcenter/TextCenterActivity.java
@@ -0,0 +1,37 @@
+package com.android.framework.demo.activity.textcenter;
+
+import android.content.Intent;
+
+import com.android.framework.base.BaseCompactActivity;
+import com.android.framework.demo.R;
+
+/**
+ * Created by meikai on 2019/06/18.
+ */
+public class TextCenterActivity extends BaseCompactActivity {
+
+    @Override
+    public int getContentViewLayoutId() {
+        return R.layout.activity_text_center;
+    }
+
+    @Override
+    public void findViews() {
+
+    }
+
+    @Override
+    public void setListeners() {
+
+    }
+
+    @Override
+    public void parseBundle(Intent intent) {
+
+    }
+
+    @Override
+    public void afterView() {
+
+    }
+}
